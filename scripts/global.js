945539890b83562a69ca44011b4b26eba0721eb1
Isaac Joás Bernardes de Lima <isaac.joas@avantia.com.br>
2022-07-31 15:23:04 -0300

    Ajustes

--- scripts/global.js
+++ scripts/global.js
@@ -10,13 +10,16 @@ class Navbar extends HTMLElement {
     }
 
     render() {
+    	
+    	const selected = this.getAttribute('data-selected');
+    	
         this.shadow.innerHTML = `
         <div style="display: flex; justify-content: space-between; padding: 1rem; align-items: center; background: rgba(125, 125, 125, 0.2);">
             <img style="height: 2rem" src="./assets/logo.svg">
-            <div style="display: flex; column-gap: .75rem; color: #FFF">
-                <span>Em Alta</span>
-                <span>Indicações</span>
-                <span>Procurar</span>
+            <div style="display: flex; column-gap: 1rem; color: #FFF; font-size: 1rem">
+                <span style="${selected == "trendings" ? 'color: #FDAD00' : ''}">Em Alta</span>
+                <span style="${selected == "recommendations" ? 'color: #FDAD00' : ''}">Indicações</span>
+                <span style="${selected == "search" ? 'color: #FDAD00' : ''}">Procurar</span>
             </div>
         </div>
         `;
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,5 @@
-var dir = tizen.filesystem;
-
 function onSuccess(files) {
     for (var i = 0; i < files.length; i++) {
-        /* Display the file path with name */
         console.log('File path and name is: ' + files[i]);
     }
 }
@@ -11,7 +8,11 @@ function onError(error) {
     console.log(error);
 }
 
-$( document ).ready(function() {
-    console.log( "ready!" );
-    tizen.filesystem.listDirectory('data', onSuccess, onError);
-});
+document.onreadystatechange = (ev) => {
+
+    if (!tizen.filesystem.pathExists("documents/profile.json")) {
+        tizen.filesystem.createFile("profile.json");
+    }
+
+    tizen.filesystem.listDirectory('documents/', onSuccess, onError);
+}
